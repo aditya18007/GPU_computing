e26d93da6219e4ebdf3176d501b9d78ec12a5e04
Aditya Singh Rathore <aditya18007@iiitd.ac.in>
2022-02-21 12:26:41 +0530

    Updated utils to create GPU and CPU arrays from raw pointer and write to them.

--- my_code/utils.h
+++ my_code/utils.h
@@ -1,12 +1,16 @@
 #ifndef MY_GPU_UTILS_H
 #define MY_GPU_UTILS_H
+
+
 #include <cuda_runtime.h>
+#include <iostream>
 
 cudaError_t my_errno ;
+
 #define SAFE_CALL( f_call, ... ) \
     my_errno = f_call(__VA_ARGS__); \
     if ( my_errno != cudaSuccess ){ \
-        std::cerr << "File : " << __FILE__ << '\n'; \
+        std::cerr << "\nFile : " << __FILE__ << '\n'; \
         std::cerr << "Function : " << __func__ << '\n'; \
         std::cerr << "Line : " << __LINE__ << '\n'; \
         std::cerr << "Cuda error : " << cudaGetErrorString(my_errno) << " !\n\n"; \
@@ -30,6 +34,14 @@ public:
         : m_size(n), m_data( new T[n])
         {}
 
+        CPU_array(T* raw_array, size_t n)
+        : m_size(n), m_data( new T[n])
+        {
+            for(size_t i = 0; i < n; i++ ){
+                m_data[i] = raw_array[i];
+            }
+        }
+
         CPU_array(GPU_array<T>& gpu_arr)
         : m_size(gpu_arr.get_size()), m_data( new T[gpu_arr.get_size()])
         {
@@ -44,12 +56,21 @@ public:
             return m_data;
         }
 
+        void write_to_ptr(T* ptr){
+            for(size_t i = 0; i m_size n; i++ ){
+                ptr[i] = m_data[i];
+            }
+        }
+        
         T& operator()(int i){
             return m_data[i];
         }
 
         size_t get_size() {
             return m_size;
+        }
+        size_t get_size_bytes() {
+            return m_size*sizeof(T);
         }
 };
 
@@ -69,6 +90,15 @@ public:
         SAFE_CALL( cudaMemcpy, m_data, cpu_arr.arr(), m_size*sizeof(T), cudaMemcpyHostToDevice )
     }
 
+    GPU_array(T* raw_ptr , size_t size): m_size(size) {
+        SAFE_CALL( cudaMalloc, (void**)&m_data , m_size * sizeof(T) )
+        SAFE_CALL( cudaMemcpy, m_data, raw_ptr, m_size*sizeof(T), cudaMemcpyHostToDevice )
+    }
+
+    void write_to_ptr(T* ptr){
+        SAFE_CALL( cudaMemcpy, ptr, m_data, m_size* sizeof(T), cudaMemcpyDeviceToHost )
+    }
+
     ~GPU_array(){
         SAFE_CALL(  cudaFree, m_data )
     }
@@ -77,6 +107,9 @@ public:
     }
     size_t get_size(){
         return m_size;
+    }
+    size_t get_size_bytes() {
+        return m_size*sizeof(T);
     }
 };
 
