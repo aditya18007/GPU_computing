9339e50a31434b3672b86bc799c60d8e71eb7e21
Aditya Singh Rathore <aditya18007@iiitd.ac.in>
2022-02-22 00:07:05 +0530

    Improved Compute Performance

--- Assignments/Assignment_2/src/sdt_gpu_kernels.cu
+++ Assignments/Assignment_2/src/sdt_gpu_kernels.cu
@@ -20,8 +20,11 @@ __global__ void compute_dist(float* min_dist, int* global_edges,int start)
 	extern __shared__ int edges[];
 	int i = threadIdx.x + blockIdx.x*blockDim.x;
 	int edge = global_edges[threadIdx.x+start];
-	edges[2*threadIdx.x] = edge % Width[0];
-	edges[2*threadIdx.x+1] = edge / Width[0];
+	int _x = edge % Width[0];
+	int _y = edge / Width[0];
+	edges[3*threadIdx.x] = _x;
+	edges[3*threadIdx.x+1] = _y;
+	edges[3*threadIdx.x+2] = _x*_x + _y*_y;
 	__syncthreads();
 
 	int sz = Sz[0];
@@ -29,14 +32,14 @@ __global__ void compute_dist(float* min_dist, int* global_edges,int start)
 	
 	int x = i%Width[0];
 	int y = i/Width[0];
+	float sqr = x*x + y*y;
 	float min = min_dist[i];
-	float _x, _y, dist2;
+	float dist2;
 	for(int k = 0; k < blockDim.x; k++){
-		_x = edges[2*k];
-		_y = edges[2*k+1];
-		dist2 = hypot(_x - x, _y - y);
+		dist2 =  edges[3*k+2] + sqr -2*(x*edges[3*k] + y*edges[3*k+1]);
 		if(dist2 < min) min = dist2;
 	}
+
 	min_dist[i] = min;
 }
 
@@ -44,7 +47,7 @@ __global__ void compute_sdt(unsigned char* bitmap, float* min_dist, float* sdt){
 	int i = threadIdx.x + blockDim.x*blockIdx.x;
 	if(i >= Sz[0]) return;
 	float sign  = (bitmap[i] >= 127)? 1.0f : -1.0f;
-    sdt[i] = sign * min_dist[i];
+    sdt[i] = sign * sqrt(min_dist[i]);
 }
 
 extern "C" void gpu_main(unsigned char* bitmap, float *sdt, int width, int height)
@@ -77,12 +80,12 @@ extern "C" void gpu_main(unsigned char* bitmap, float *sdt, int width, int heigh
 	const auto grid_size = (sz/block_size) + 1;
 	const auto grid_last_chunk = (sz/last_chunk) + 1;
 	for(int i = 0; i < num_chunks; i++){
-		compute_dist<<< grid_size, block_size, 2*chunk_size*sizeof(int)>>>(d_min_dist.arr(), 
+		compute_dist<<< grid_size, block_size, 3*chunk_size*sizeof(int)>>>(d_min_dist.arr(), 
 		d_edges.arr(), 
 		i*chunk_size);
 	}
 	if (last_chunk != 0){
-		compute_dist<<< grid_last_chunk, last_chunk, 2*last_chunk*sizeof(int)>>>(d_min_dist.arr(),
+		compute_dist<<< grid_last_chunk, last_chunk, 3*last_chunk*sizeof(int)>>>(d_min_dist.arr(),
 			d_edges.arr(),
 			num_chunks*chunk_size);
 	}
